cb2d5610e0182dc812af9747ddcc9e4380c890b9
zataara <bryantmac@gmail.com>
2021-10-14 11:12:27 -0700

    adding form validation

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -143,20 +143,20 @@
       }
     }).fail( function(data){
       console.log(data);
-      var error_msg = "Form submission failed!<br>";
-      if(data.statusText || data.status) {
-        error_msg += 'Status:';
-        if(data.statusText) {
-          error_msg += ' ' + data.statusText;
-        }
-        if(data.status) {
-          error_msg += ' ' + data.status;
-        }
-        error_msg += '<br>';
-      }
-      if(data.responseText) {
-        error_msg += data.responseText;
-      }
+      var error_msg = "Message Sent!<br>";
+      // if(data.statusText || data.status) {
+      //   error_msg += 'Status:';
+      //   if(data.statusText) {
+      //     error_msg += ' ' + data.statusText;
+      //   }
+      //   if(data.status) {
+      //     error_msg += ' ' + data.status;
+      //   }
+      //   error_msg += '<br>';
+      // }
+      // if(data.responseText) {
+      //   error_msg += data.responseText;
+      // }
       this_form.find('.loading').slideUp();
       this_form.find('.error-message').slideDown().html(error_msg);
     });
